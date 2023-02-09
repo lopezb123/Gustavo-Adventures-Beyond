f289012462ad6b0b831ef0ee2942381c3916e3b0
KarsonHyland <78519480+KarsonHyland@users.noreply.github.com>
2023-02-08 20:58:56 -0600

    Ollie Animation Working with Jump
    Animation Looks goofy af, will fix that later!

--- Gustavo Adventures Beyond/Assets/Scripts/BoardAnimations.cs
+++ Gustavo Adventures Beyond/Assets/Scripts/BoardAnimations.cs
@@ -15,7 +15,7 @@ public class BoardAnimations : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.L)){
+        if(Input.GetKeyDown(KeyCode.Space)){
             animator.SetTrigger("OllieTrigger");
         }
         else
