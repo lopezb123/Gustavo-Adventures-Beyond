a3bbfe5f91adfe72c8100551545e7afe5362ff31
TylerMooney <71684071+TylerMooney@users.noreply.github.com>
2023-05-01 21:36:44 -0500

    Add Soda Can

---
+++ Gustavo Adventures Beyond/Assets/Scripts/SodaController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SodaController : MonoBehaviour
+{
+
+    [SerializeField] GameObject scoreTracker;
+    // Start is called before the first frame update
+
+}
